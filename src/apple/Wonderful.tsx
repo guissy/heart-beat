80afddbf505efe9184a3233727ec91f5b652fe64
桂子 <guissy@qq.com>
2019-09-17 00:44:54 +0800

    animation

--- src/apple/Wonderful.tsx
+++ src/apple/Wonderful.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import h1 from './assets/h1.png';
 import './Wonderful.css';
-import { Tween } from 'react-gsap';
+import { Tween, Timeline } from 'react-gsap';
 // @ts-ignore
 import { Power2 } from 'gsap/TweenMax';
 
@@ -12,21 +12,56 @@ interface Props {
 const Wonderful: React.FC<Props> = ({ }) => {
   return (
     <section className="wonderful">
-      <div className="h1-box">
-      <Tween
-        to={{
-            scale: 0.14
-        }}
-        duration={1}
-        ease={Power2.easeOut}
-      >
-        <div className="h1-wrap">
-          <img src={h1} alt="" />
+      <Timeline target={
+        <div className="h1-box">
+          <Tween
+            delay={4}
+            to={{
+              scale: 0.1,
+              borderRadius: '50%',
+              padding: '42%',
+            }}
+            duration={1.2}
+            ease={Power2.easeIn}
+          >
+            <div className="h1-wrap">
+              <img src={h1} alt="" />
+            </div>
+          </Tween>
         </div>
-      </Tween>
-      </div>
-
-    </section>
+      }>
+        <Tween
+          delay={2}
+          from={{
+            scale: 3,
+          }}
+          to={{
+            scale: 1.8,
+          }}
+          duration={1}
+          ease={Power2.easeIn}
+        />
+        <Tween
+          from={{ backgroundColor: "#ffffff", }}
+          to={{
+            scale: 1,
+            backgroundColor: '#e4e3e4'
+          }}
+          duration={1}
+          ease={Power2.easeOut}
+        />
+        <Tween
+          from={{ borderRadius: 0 }}
+          to={{
+            width: 436,
+            height: 436,
+            borderRadius: '50%'
+          }}
+          duration={1}
+          ease={Power2.easeOut}
+        />
+      </Timeline>
+    </section >
   );
 };
 
